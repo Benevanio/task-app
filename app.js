7b81b6ecaa9c324220d4289e1c652ec4354a7649
Benevanio <benevaniosantos930@gmail.com>
2025-04-01 18:53:33 -0300

    feat: add user registration endpoint and user model

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-
+const UserSchema = require('./model/model');
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -23,6 +23,17 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new UserSchema({ name, email, password });
+    await user.save();
+    res.status(201).send('User registered successfully');
+  } catch (error) {
+    res.status(400).send('Error registering user');
+  }
 });
 
 app.listen(process.env.PORT, () => {
