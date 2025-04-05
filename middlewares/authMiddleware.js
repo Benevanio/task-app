64d039cd4b4fbd00b3072402e61dbb783fa785d9
Benevanio <benevaniosantos930@gmail.com>
2025-04-05 16:50:49 -0300

    fix: melhorar mensagens de erro no middleware de autenticação

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,14 +4,14 @@ dotenv.config();
 const authMiddleware = (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: 'Unauthorized' , error: 'No token provided' });
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
-        return res.status(403).json({ message: 'Forbidden' });
+        return res.status(403).json({ message: 'Forbidden', error });
     }
 }
 module.exports = authMiddleware;
