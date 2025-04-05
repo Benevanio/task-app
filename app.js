5c432bb4ed42cd17bc337b448ef02c4bfec001d6
Benevanio <benevaniosantos930@gmail.com>
2025-04-05 08:32:26 -0300

    feat: adicionar suporte a hash de senhas com bcrypt ao registro de usuários

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
 
 const app = express();
 const UserSchema = require('./model/userModel');
 const TaskSchema = require('./model/taskModel');
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log('MongoDB connected');
   })
@@ -29,9 +29,11 @@ app.get('/', (req, res) => {
 app.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    const user = new UserSchema({ name, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new UserSchema({ name, email, password: hashedPassword });
     await user.save();
     res.status(201).send('User registered successfully');
+
   } catch (error) {
     res.status(400).send('Error registering user'+ error);
   }
